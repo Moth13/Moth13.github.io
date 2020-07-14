e3dfd7364bd69bcb4c36ec46c6be421cf86a9baa
Jérémie Guérinel <jguerinel@ifeelsmart.com>
2020-07-14 18:13:45 +0200

    first resume changes.

---
+++ precache-manifest.519809d0629ea2d659101e80b6624ed5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6b130edefee7b6dc6a9aae69a1b920f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c946638d2257419e8374",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "871b2a98cee7a6abe712",
+    "url": "/static/js/2.56ac96b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56ac96b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c946638d2257419e8374",
+    "url": "/static/js/main.aff6a961.chunk.js"
+  },
+  {
+    "revision": "4885984d50a42209b6bb",
+    "url": "/static/js/runtime-main.f5e9834c.js"
+  }
+]);
